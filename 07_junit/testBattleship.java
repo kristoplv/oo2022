4fb9c6be18cb438c249cddc6a948e45a3a486a24
Kristo Põlluvee <89929763+kristoplv@users.noreply.github.com>
2022-04-04 09:47:37 +0300

    uuendused testile

--- 07_junit/testBattleship.java
+++ 07_junit/testBattleship.java
@@ -20,4 +20,40 @@ public class testBattleship {
         assertEquals(1, pommitusKontroll[0]);
         assertEquals(1, pommitusKontroll[1]);
     }
+    @Test public void laevaYonOlemas() {
+        tuum.asetaLaev();
+        assertNotNull("laeval peab olema määratud Y", tuum.laevaY());
+    }
+
+    @Test public void laevaHpOnOlemas() {
+        tuum.asetaLaev();
+        assertNotNull("laeval peab olema määratud HP", tuum.returnHealth());
+    }
+
+    @Test public void laevaHpVaheneb() {
+        tuum.asetaLaev();
+        int HP, uusHP;
+        HP = tuum.returnHealth();
+        tuum.damageShip();
+        uusHP = tuum.returnHealth();
+        assertEquals(HP - 1, uusHP);
+    }
+    @Test public void testTulemus() {
+        tuum.asetaLaev();
+        int laevX = tuum.laevaX();
+        int laevY = tuum.laevaY();
+        tuum.pommitus(laevX + 1, laevY + 1);
+        assertEquals("moodas", tuum.tulemus());
+        tuum.pommitus(laevX, laevY);
+        assertEquals("pihtas", tuum.tulemus());
+    }
+    @Test public void laevaXolemas(){
+        tuum.asetaLaev();
+        assertNotNull("laeval peab olema X", tuum.laevaX());
+    }
+
+    @Test public void laevaKohtOlemas(){
+        tuum.laevaKoht();
+        assertNotNull("laeval peab olema koht", tuum.laevaKoht());
+    }
 }
