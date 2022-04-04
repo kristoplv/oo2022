5599e93a722920a4acb8e19ccdb1184371c91a64
Kristo Põlluvee <89929763+kristoplv@users.noreply.github.com>
2022-04-04 12:51:45 +0300

    test alamklassid

---
+++ 08_alamklassid/test1/Punktid4.java
@@ -0,0 +1,8 @@
+public class Punktid4{
+    public static void main(String[] argumendid){
+      Punkt4 p=new Punkt4(3, 4);
+      System.out.println(p.kirjutaAndmed());
+      System.out.println("Kaugus koordinaatide alguspunktist= "+
+                          p.kaugusNullist());
+    }
+  }
